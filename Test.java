cff5258ec91b97e490522fed066ddadda255aadb
Flowkey <wawadish@gmail.com>
2018-03-03 16:45:47 -0500

    Merged Zimu and Walid's test branch
    Merging test

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+    private int a;
+    public Test(int a){
+        this.a = a;
+    }
+    public void doubleNumber(int i){
+        i = i*2;
+        System.out.println("The value of i in the class Test is: " + i);
+    }
+}
---
+++ testZimu.java
@@ -0,0 +1,9 @@
+public class testZimu {
+
+    public static void main(String[] args) {
+		Test t = new Test(12);
+
+		t.doubleNumber(t.a);
+    }
+
+}
